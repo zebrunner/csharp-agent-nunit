cd11e1eb1b7c66b168c8db194e2e9b1e80850b16
m.li <mohamad.li.dev@gmail.com>
2018-04-10 21:52:16 +0300

    updated zafira properties initialization

--- listener/ZafiraListener.cs
+++ listener/ZafiraListener.cs
@@ -12,26 +12,26 @@ namespace ZafiraIntegration
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
         private static string ANONYMOUS = "anonymous";
-        private static string JIRA_SUITE_ID = getString("jira_suite_id", null);
-        private static bool ZAFIRA_ENABLED = getBoolean("zafira_enabled", true);
-        private static string ZAFIRA_URL = getString("zafira_service_url", "http://demo.qaprosoft.com/zafira-ws");
-        private static string ZAFIRA_ACCESS_TOKEN = getString("zafira_access_token", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwicGFzc3dvcmQiOiIySDl5ZVhNcWoxb1lLVm1WZlYxY28vZ3ZYdmRHejdxTiIsImV4cCI6MTMwMzY3NTM4MTk1fQ.yqp4BJd7OpgX7aOdQOjGKdYb2DvHK2ds6ilc0MoO6p_vkbZhkjIK-eCr8dhT7Riwj8x6ru0Lup6Zj-FithCfOw");
-        private static string ZAFIRA_PROJECT = getString("zafira_project", "RomaTest");
-        private static string ZAFIRA_REPORT_EMAILS = getString("zafira_report_emails", "test@gmail.com").Trim().Replace(" ", ",").Replace(";", ",");
-        private static string ZAFIRA_REPORT_FOLDER = getString("zafira_report_folder", "FOLDER_PATH");
-        private static bool ZAFIRA_RERUN_FAILURES = getBoolean("zafira_rerun_failures", false);
-        private static bool ZAFIRA_REPORT_SHOW_STACKTRACE = getBoolean("zafira_report_show_stacktrace", true);
-        private static bool ZAFIRA_REPORT_SHOW_FAILURES_ONLY = getBoolean("zafira_report_failures_only", false);
-        private static string ZAFIRA_CONFIGURATOR = getString("zafira_configurator", "com.qaprosoft.zafira.listener.DefaultConfigurator");
+        private string JIRA_SUITE_ID;
+        private bool ZAFIRA_ENABLED;
+        private string ZAFIRA_URL;
+        private string ZAFIRA_ACCESS_TOKEN;
+        private string ZAFIRA_PROJECT;
+        private string ZAFIRA_REPORT_EMAILS;
+        private string ZAFIRA_REPORT_FOLDER;
+        private bool ZAFIRA_RERUN_FAILURES;
+        private bool ZAFIRA_REPORT_SHOW_STACKTRACE;
+        private bool ZAFIRA_REPORT_SHOW_FAILURES_ONLY;
+        private string ZAFIRA_CONFIGURATOR;
         private CIConfig ci;
         private ZafiraClient zc;
-        private UserType user = null;
-        private JobType parentJob = null;
-        private JobType job = null;
-        private TestSuiteType suite = null;
-        private TestRunType run = null;
-        private TestType test = null;
-        private TestCaseType testCase = null;
+        private UserType user;
+        private JobType parentJob;
+        private JobType job;
+        private TestSuiteType suite;
+        private TestRunType run;
+        private TestType test;
+        private TestCaseType testCase;
 
         [OneTimeSetUp]
         public void onStart()
@@ -123,6 +123,18 @@ namespace ZafiraIntegration
                 ci.setGitBranch(getString("git_branch", null));
                 ci.setGitCommit(getString("git_commit", null));
                 ci.setGitUrl(getString("git_url", null));
+
+                JIRA_SUITE_ID = getString("jira_suite_id", null);
+                ZAFIRA_ENABLED = getBoolean("zafira_enabled", false);
+                ZAFIRA_URL = getString("zafira_service_url", "http://demo.qaprosoft.com/zafira-ws");
+                ZAFIRA_ACCESS_TOKEN = getString("zafira_access_token", "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwicGFzc3dvcmQiOiIySDl5ZVhNcWoxb1lLVm1WZlYxY28vZ3ZYdmRHejdxTiIsImV4cCI6MTMwMzY3NTM4MTk1fQ.yqp4BJd7OpgX7aOdQOjGKdYb2DvHK2ds6ilc0MoO6p_vkbZhkjIK-eCr8dhT7Riwj8x6ru0Lup6Zj-FithCfOw");
+                ZAFIRA_PROJECT = getString("zafira_project", "DemoTest");
+                ZAFIRA_REPORT_EMAILS = getString("zafira_report_emails", "demoqaprosoft@gmail.com").Trim().Replace(" ", ",").Replace(";", ",");
+                ZAFIRA_REPORT_FOLDER = getString("zafira_report_folder", "FOLDER_PATH");
+                ZAFIRA_RERUN_FAILURES = getBoolean("zafira_rerun_failures", false);
+                ZAFIRA_REPORT_SHOW_STACKTRACE = getBoolean("zafira_report_show_stacktrace", true);
+                ZAFIRA_REPORT_SHOW_FAILURES_ONLY = getBoolean("zafira_report_failures_only", false);
+                ZAFIRA_CONFIGURATOR = getString("zafira_configurator", "com.qaprosoft.zafira.listener.DefaultConfigurator");
 
                 if (ZAFIRA_ENABLED)
                 {
